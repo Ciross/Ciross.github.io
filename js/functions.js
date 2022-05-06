5d83a0d70a15a3fcf848026ee28464132b582f89
Biioz <105040939+Biioz@users.noreply.github.com>
2022-05-06 09:34:37 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,23 @@
-
+function search() {
+	const games = ["Zelda", "Sonic", "Mario", "COD", "Star wars", "Switch sports", "Elden ring", "Sony"]
+	let list = games;
+	let txt = document.getElementById('searchbar').value;
+	let result = [];
+	let i = -1;
+	for(let k = 0; k < txt.length; k++) {
+		i++
+		let e = 0
+		for(let z = 0; z < list.length; z++) {
+			if(txt[k] === list[e][i]) {
+				result.push(list[e])
+				e++
+			} else {
+				list.splice(e, 1)
+			};
+		};
+	};
+	if(result !== []) {
+		console.log(result)
+	};
+	return 'None'
+};
